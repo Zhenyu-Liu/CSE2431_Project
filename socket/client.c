80772279a662aa566549e678512acebc4e5012eb
JoannaSiFang <sifang95@gmail.com>
2016-11-29 19:25:50 -0500

    switch function of client and server

--- socket/client.c
+++ socket/client.c
@@ -1,64 +1,114 @@
-/*
-    C client example using sockets
-*/
+/* getfile server that handles only one client at a time */
+#include <stdio.h> /* printf and standard i/o */
+#include <sys/socket.h> /* socket, bind, listen, accept, socklen_t */
+#include <arpa/inet.h> /* sockaddr_in, inet_ntop */
+#include <string.h> /* strlen */
+#include <stdlib.h> /* atoi, EXIT_FAILURE */
+#include <fcntl.h> /* open, O_RDONLY */
+#include <unistd.h> /* close, read */
+#define SRV_PORT 5103 /* default port number */
+#define MAX_RECV_BUF 256
+#define MAX_SEND_BUF 256
+char* SRV_IP = "10.0.2.15";
+void get_file_name(int, char*);
+int send_file(int , char*);
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>       // strlen
-#include <sys/socket.h>   // socket
-#include <arpa/inet.h>    // inet_addr
-#include <unistd.h>       // write
-#include <fcntl.h>
-
-int main(int argc , char *argv[])
+int main(int argc, char* argv[])
 {
-    // Variables
-    int sock, read_size, ret;
-    struct sockaddr_in server;
-    char message[2000];
+ int sock_fd;
+ struct sockaddr_in srv_addr;
+ char file_name [MAX_RECV_BUF]; /* name of the file to be sent */
 
-    char *addr = "127.0.0.1";
-    short port = 8888;
+ memset(&srv_addr, 0, sizeof(srv_addr)); /* zero-fill srv_addr structure*/
 
-    // Create a socket. Return value is a file descriptor for the socket.
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        perror("Error creating socket\n");
-        exit(0);
-    }
-    printf("Client socket created\n");
+ /* create a client socket */
+ sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+ srv_addr.sin_family = AF_INET; /* internet address family */
 
-    // Set the server ip address, connection family and port
-    server.sin_addr.s_addr = inet_addr(addr);
-    server.sin_family      = AF_INET;
-    server.sin_port        = htons(port);
+ /* convert command line argument to numeric IP */
+ if ( inet_pton(AF_INET, SRV_IP, &(srv_addr.sin_addr)) < 1 )
+ {
+ printf("Invalid IP address\n");
+exit(EXIT_FAILURE);
+ }
 
-    // Connect to remote server
-    if (connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
-        perror("Error establishing connection\n");
-        close(sock);
-        exit(0);
-    }
-    printf("Connecting to server...\n");
+ /* if port number supplied, use it, otherwise use SRV_PORT */
+ srv_addr.sin_port = (argc > 1) ? htons(atoi(argv[1])) : htons(SRV_PORT);
 
-    printf("Enter message: ");
-    scanf("%s" , message);
+ if( connect(sock_fd, (struct sockaddr*) &srv_addr, sizeof(srv_addr)) < 0 )
+ {
+ perror("connect error");
+ exit(EXIT_FAILURE);
+ }
+ printf("connected to:%s:%d ..\n",SRV_IP,SRV_PORT);
 
-    if ((ret = write(sock, message, strlen(message))) <= 0) {
-        perror("Error writing\n");
-        close(sock);
-        exit(0);
-    }
-    // Receive a message from server
-    if ((read_size = read(sock, message, sizeof(message))) > 0) {
-        printf("Server: %s\n", message);
-    } else if(read_size == 0) {
-        printf("Server disconnected\n");
-        fflush(stdout);
-    } else if(read_size == -1) {
-        perror("Error receiving message from server\n");
-    }
+ get_file_name(sock_fd, file_name);
+ send_file(sock_fd, file_name);
 
-    close(sock);
-    return 0;
+ /* close socket*/
+ if(close(sock_fd) < 0)
+ {
+    perror("socket close error");
+ exit(EXIT_FAILURE);
+ }
+ return 0;
+}
+
+
+void get_file_name(int sock, char* file_name)
+{
+ char recv_str[MAX_RECV_BUF]; /* to store received string */
+ ssize_t rcvd_bytes; /* bytes received from socket */
+
+ /* read name of requested file from socket */
+ if ( (rcvd_bytes = recv(sock, recv_str, MAX_RECV_BUF, 0)) < 0) {
+ perror("recv error");
+ return;
+ }
+
+ sscanf (recv_str, "%s\n", file_name); /* discard CR/LF */
+}
+
+int send_file(int sock, char *file_name)
+{
+ int sent_count; /* how many sending chunks, for debugging */
+ ssize_t read_bytes, /* bytes read from local file */
+ sent_bytes, /* bytes sent to connected socket */
+ sent_file_size;
+ char send_buf[MAX_SEND_BUF]; /* max chunk size for sending file */
+ char * errmsg_notfound = "File not found\n";
+ int f; /* file handle for reading local file*/
+ sent_count = 0;
+ sent_file_size = 0;
+ /* attempt to open requested file for reading */
+ if( (f = open(file_name, O_RDONLY)) < 0) /* can't open requested file */
+ {
+   perror(file_name);
+   if( (sent_bytes = send(sock, errmsg_notfound ,
+                          strlen(errmsg_notfound), 0)) < 0 )
+     {
+       perror("send error");
+       return -1;
+     }
+ }
+ else /* open file successful */
+   {
+     printf("Sending file: %s\n", file_name);
+     while( (read_bytes = read(f, send_buf, MAX_RECV_BUF)) > 0 )
+       {
+         if( (sent_bytes = send(sock, send_buf, read_bytes, 0))
+             < read_bytes )
+           {
+             perror("send error");
+             return -1;
+           }
+         sent_count++;
+         sent_file_size += sent_bytes;
+       }
+     close(f);
+   } /* end else */
+
+ printf("Done with this client. Sent %d bytes in %d send(s)\n\n",
+        sent_file_size, sent_count);
+ return sent_count;
 }
--- socket/server.c
+++ socket/server.c
@@ -1,112 +1,114 @@
-/*
-    C socket server example
-*/
+/* getfile server that handles only one client at a time */
+#include <stdio.h> /* printf and standard i/o */
+#include <sys/socket.h> /* socket, bind, listen, accept, socklen_t */
+#include <arpa/inet.h> /* sockaddr_in, inet_ntop */
+#include <string.h> /* strlen */
+#include <stdlib.h> /* atoi, EXIT_FAILURE */
+#include <fcntl.h> /* open, O_RDONLY */
+#include <unistd.h> /* close, read */
+#define SRV_PORT 5103 /* default port number */
+#define LISTEN_ENQ 5 /* for listen backlog */
+#define MAX_RECV_BUF 256
+#define MAX_SEND_BUF 256
+int recv_file(int ,char*);
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>      // strlen
-#include <sys/socket.h>  // socket
-#include <arpa/inet.h>   // inet_addr
-#include <unistd.h>      // write
-
-void swap(char* a, char* b) {
-    *a = (*a) ^ (*b);
-    *b = (*a) ^ (*b);
-    *a = (*a) ^ (*b);
-}
-
-void toggle_capitalization(char* ch) {
-    if (*ch >= 97 && *ch <= 122) {
-        // a to z
-        *ch = (*ch) - 32;
-    } else if (*ch >= 65 && *ch <= 90) {
-        // A to Z
-        *ch = (*ch) + 32;
-    }
-}
-
-void reverse_order(char* str) {
-    int len = strlen(str);
-    int idx = 0;
-    for (; idx < len / 2; idx++) {
-        swap(str + idx, str + len - idx - 1);
-    }
-}
-
-void reverse_capitalization(char* str) {
-    int len = strlen(str);
-    int idx = 0;
-    for (; idx < len; idx++) {
-        toggle_capitalization(str + idx);
-    }
-}
-
-int main(int argc , char *argv[])
+int main(int argc, char* argv[])
 {
-    // Variables
-    int ssock, csock, addrlen, read_size, ret;
-    struct sockaddr_in server, client;
-    char message[2000];
+ int listen_fd, conn_fd;
+ struct sockaddr_in srv_addr, cli_addr;
+ socklen_t cli_len;
+ char print_addr [INET_ADDRSTRLEN]; /* readable IP address */
+ if (argc < 2){
+ 	printf("usage: %s <filename> [port number]\n", argv[0]);
+	exit(EXIT_FAILURE);
+ }
+ memset(&srv_addr, 0, sizeof(srv_addr)); /* zero-fill srv_addr structure*/
+ memset(&cli_addr, 0, sizeof(cli_addr)); /* zero-fill cli_addr structure*/
+ srv_addr.sin_family = AF_INET;
+ srv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+ /* if port number supplied, use it, otherwise use SRV_PORT */
+ srv_addr.sin_port = (argc > 2) ? htons(atoi(argv[2])) : htons(SRV_PORT);
+ if ( (listen_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+ perror("socket error");
+ exit(EXIT_FAILURE);
+ }
+ /* bind to created socket */
+ if( bind(listen_fd, (struct sockaddr*) &srv_addr, sizeof(srv_addr)) < 0 ){
+ perror("bind error");
+ exit(EXIT_FAILURE);
+ }
 
-    short port = 8888;
+ printf("Listening on port number %d ...\n", ntohs(srv_addr.sin_port));
+ if( listen(listen_fd, LISTEN_ENQ) < 0 ) {
+ perror("listen error");
+ exit(EXIT_FAILURE);
+ }
 
-    // Create a socket. Return value is a file descriptor for the socket.
-    ssock = socket(AF_INET, SOCK_STREAM, 0);
-    if (ssock == -1) {
-        perror("Error creating socket\n");
-        exit(0);
-    }
-    printf("Server socket created\n");
+ for( ; ; ) /* run forever*/
+ {
+ cli_len = sizeof(cli_addr);
+ printf ("Waiting for a client to connect...\n\n");
+ /* block until some client connects */
+ if ( (conn_fd = accept(listen_fd, (struct sockaddr*) &cli_addr,
+ &cli_len)) < 0 )
+ {
+ perror("accept error");
+ break; /* exit from the for loop */
+ }
 
-    // Set the server ip address, connection family and port. INADDR_ANY means
-    // all the ip addresses of the server can be used to set up connection.
-    server.sin_family      = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port        = htons(port);
+ /* convert numeric IP to readable format for displaying */
+ inet_ntop(AF_INET, &(cli_addr.sin_addr), print_addr, INET_ADDRSTRLEN);
+ printf("Client connected from %s:%d\n",
+ print_addr, ntohs(cli_addr.sin_port) );
 
-    // Bind the socket for the connection
-    if (bind(ssock, (struct sockaddr *) &server, sizeof(server)) < 0) {
-        //print the error message
-        perror("Error binding\n");
-        close(ssock);
-        exit(0);
-    }
-    printf("Binding done\n");
+ recv_file(conn_fd, argv[1]); /* argv[1] = file name */
+ printf("Closing connection\n");
+ close(conn_fd); /* close connected socket*/
+ } /* end for */
 
-    // Listen for incoming connections
-    listen(ssock, SOMAXCONN);
-    printf("Listening...\n");
+ close(listen_fd); /* close listening socket*/
+ return 0;
+}
 
-    printf("Waiting for incoming connections...\n");
-    addrlen = sizeof(struct sockaddr_in);
+int recv_file(int sock, char* file_name)
+{
+ char send_str [MAX_SEND_BUF]; /* message to be sent to server*/
+ int f; /* file handle for receiving file*/
+ ssize_t sent_bytes, rcvd_bytes, rcvd_file_size;
+ int recv_count; /* count of recv() calls*/
+ char recv_str[MAX_RECV_BUF]; /* buffer to hold received data */
+ size_t send_strlen; /* length of transmitted string */
 
-    // Accept connection from an incoming client
-    csock = accept(ssock, (struct sockaddr *) &client, (socklen_t *) &addrlen);
-    if (csock < 0) {
-        perror("Error accepting connections\n");
-        exit(0);
-    }
-    printf("Connection accepted\n");
+ sprintf(send_str, "%s\n", file_name); /* add CR/LF (new line) */
+ send_strlen = strlen(send_str); /* length of message to be transmitted */
+ if( (sent_bytes = send(sock, file_name, send_strlen, 0)) < 0 ) {
+ perror("send error");
+ return -1;
+ }
+ /* attempt to create file to save received data. 0644 = rw-r--r-- */
+ if ( (f = open(file_name, O_WRONLY|O_CREAT, 0644)) < 0 )
+ {
+ perror("error creating file");
+ return -1;
+ }
 
-    memset(message, 0, sizeof(message));
-    // Receive a message from the client. recv() is an alternative for read().
-    // Similarly, send() can replace write().
-    if ((read_size = read(csock, message, sizeof(message))) > 0) {
-        printf("Client: %s\n", message);
-        reverse_order(message);
-        reverse_capitalization(message);
-        printf("After processing: %s\n", message);
-        if ((ret = write(csock, message, strlen(message))) <= 0) {
-            perror("Error writing\n");
-        }
-    } else {
-        perror("Error receiving\n");
-    }
+ recv_count = 0; /* number of recv() calls required to receive the file */
+ rcvd_file_size = 0; /* size of received file */
 
+ /* continue receiving until ? (data or close) */
+ while ( (rcvd_bytes = recv(sock, recv_str, MAX_RECV_BUF, 0)) > 0 )
+ {
+ recv_count++;
+ rcvd_file_size += rcvd_bytes;
 
-    close(csock);
-    close(ssock);
-    printf("End connection\n");
-
-    return 0;
+ if (write(f, recv_str, rcvd_bytes) < 0 )
+ {
+ perror("error writing to file");
+ return -1;
+ }
+ }
+ close(f); /* close file*/
+ printf("Client Received: %d bytes in %d recv(s)\n", rcvd_file_size,
+ recv_count);
+ return rcvd_file_size;
 }
